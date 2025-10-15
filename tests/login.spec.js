150eae0c237844ba9e4cb7a5274ecee6caecb40d
RafsanaKabirSamanta <rafsana@ibos.com>
2025-10-15 11:07:10 +0600

    feat: login functionality Completed

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -1,12 +1,13 @@
-// @ts-check
-import { test } from '@playwright/test';
-import { loginToManagerium, verifyWelcomeMessage } from '../utility-pages/login-utils.js';
+import { test, expect } from '@playwright/test';
 
-test('successful login to Managerium', async ({ page }) => {
-    await loginToManagerium(page);
-});
+test('test', async ({ page }) => {
+  await page.goto('https://devmgm.ibos.io/');
+  await page.getByRole('textbox', { name: 'Enter your mobile no' }).click();
+  await page.getByRole('textbox', { name: 'Enter your mobile no' }).fill('01679172828');
+  await page.getByRole('textbox', { name: 'Enter your mobile no' }).press('Tab');
+  await page.getByRole('textbox', { name: 'Enter your password' }).click();
+  await page.getByRole('textbox', { name: 'Enter your password' }).fill('ibos@123');
+  await page.getByRole('button', { name: 'LOG IN' }).click();
+  await page.getByText('Welcome to Managerium,').dblclick();
 
-test('verify welcome message after login', async ({ page }) => {
-    await loginToManagerium(page);
-    await verifyWelcomeMessage(page);
 });
